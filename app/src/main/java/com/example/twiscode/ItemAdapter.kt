c3d1e37c496357ca794b8150556828e1a0b8772f
megamusfiva <musfivawati@gmail.com>
2021-09-03 22:39:31 +0700

    UI

---
+++ app/src/main/java/com/example/twiscode/ItemAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.twiscode
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ItemAdapter() {
+
+}
--- app/src/main/java/com/example/twiscode/MainActivity.kt
+++ app/src/main/java/com/example/twiscode/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.twiscode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvItem: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvItem.layoutManager = GridLayoutManager(this, 2)
     }
+
 }
